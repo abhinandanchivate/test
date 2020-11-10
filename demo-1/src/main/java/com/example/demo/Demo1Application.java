8ea82ec6b53c0f17f9bbe0d488512dfd9e38c94a
abhin <abhinandanchivate2000@gmail.com>
2020-11-10 18:44:16 +0530

    add sysout

--- demo-1/src/main/java/com/example/demo/Demo1Application.java
+++ demo-1/src/main/java/com/example/demo/Demo1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
+		println("hello from shree");
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
